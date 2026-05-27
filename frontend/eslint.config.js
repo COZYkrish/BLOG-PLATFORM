a836d66cfba1f86351b9836758db5cf83ef7a89e
COZYkrish <cozykrish2916@gmail.com>
2026-05-27 10:00:00 +0530

    feat: project updates

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -7,12 +7,12 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    // files: ['**/*.{js,jsx}'],
-    // extends: [
-    //   js.configs.recommended,
-    //   reactHooks.configs.flat.recommended,
-    //   reactRefresh.configs.vite,
-    // ],
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
     // languageOptions: {
     //   globals: globals.browser,
     //   parserOptions: { ecmaFeatures: { jsx: true } },
